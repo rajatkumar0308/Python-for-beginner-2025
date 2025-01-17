fdd6cd3b0a878f0bf6ed8694de9515ccf7aab085
rajatkumar0308 <rajat.kumar03081993@gmail.com>
2025-01-17 17:44:43 +0530

    Add files via upload

---
+++ selection control.py
@@ -0,0 +1,19 @@
+# if , wlif , else
+#x = int(input("Eneter your marks"))
+#if x > 75:
+#    print("You are pass with honor degree")
+#elif x > 65 and x <=75 :
+#    print("You are first class degree")
+#elif x > 55 and x <=65 :
+#    print("you are second class degree")         
+#else:
+#    print("Sorry you are not honor degree")     
+
+# nested if 
+y = int(input("Give your marks"))
+name = input("tell me your name")
+if y == 10:
+    if name == "rajat":
+        print("You are authenticated student")
+else:
+    print("You are a thug")     
