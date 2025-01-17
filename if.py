fbfcdd22a4be5e40108f92be191fbc9e8a458e66
rajatkumar0308 <rajat.kumar03081993@gmail.com>
2025-01-17 14:53:29 +0530

    Add files via upload

---
+++ if.py
@@ -0,0 +1,10 @@
+# if - elif - else 
+
+number = 111
+
+if number > 15:
+    print("The number is greater than 15")
+elif  number == 10:
+    print("The number is exact match")
+else:
+    print("the number is not here")       
